a4fa7bc3cd6c8462fcdaaf397a7ab5f6b7ae609a
Azibek44 <azibektasbolotuulu@gmail.com>
2023-09-22 17:56:01 +0600

    ToDo

---
+++ apps/ToDo/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from .models import Task
+from .serializer import TaskSerializer
+
+class ToDoAPI(GenericViewSet,
+              mixins.ListModelMixin,
+              mixins.CreateModelMixin,
+              mixins.RetrieveModelMixin,
+              mixins.DestroyModelMixin,
+              mixins.UpdateModelMixin):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
